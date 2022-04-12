72e78010d4f0d135e0293e3066b46029995e55e3
Vadim Bowbel <vadim.bowbel@outlook.de>
2022-04-12 16:23:28 +0200

    Initial commit

---
+++ src/components/Layout.js
@@ -0,0 +1,141 @@
+import { Button, makeStyles } from '@material-ui/core'
+import React from 'react'
+import { Drawer } from '@material-ui/core'
+import { Typography } from '@material-ui/core'
+import { List } from '@material-ui/core'
+import { ListItem } from '@material-ui/core'
+import { ListItemIcon } from '@material-ui/core'
+import { ListItemText } from '@material-ui/core'
+import { AddCircleOutlineOutlined, SubjectOutlined } from '@material-ui/icons'
+import { useHistory } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
+import { AppBar } from '@material-ui/core'
+import { Toolbar } from '@material-ui/core'
+import { format } from 'date-fns'
+import { Avatar } from '@material-ui/core'
+import { Login } from '@mui/icons-material'
+
+const drawerWidth = 240
+
+const useStyles = makeStyles((theme) =>{
+    return {
+        page: {
+            background: "#f9f9f9",
+            width: "100%",
+            padding: theme.spacing(3)
+        },
+        drawer: {
+            width: drawerWidth
+        },
+        drawerPaper: {
+            width: drawerWidth
+        },
+        root: {
+            display: "flex"
+        },
+        active: {
+            background: '#f4f4f4'
+        },
+        title: {
+            padding: theme.spacing(2),
+            display: "flex"
+        },
+        appbar: {
+            width: `calc(100% - ${drawerWidth}px)`
+        },
+        toolbar: theme.mixins.toolbar,
+        date: {
+            flexGrow: 1
+        },
+        avatar: {
+            marginLeft: theme.spacing(1),
+        }
+    }
+    
+})
+
+export default function Layout({ children }) {
+    const classes = useStyles()
+    const history = useHistory()
+    const location = useLocation()
+
+    const menuItems = [
+        {
+            text: 'Login',
+            icon: <Login color="secondary" />,
+            path: '/'
+        },
+        {
+            text: 'My Workouts',
+            icon: <SubjectOutlined color="secondary" />,
+            path: '/'
+        },
+        {
+            text: 'Create Workout',
+            icon: <AddCircleOutlineOutlined color="secondary" />,
+            path: '/create'
+        }
+    ]
+
+    return (
+    <div className={classes.root}>
+        {/* app bar */}
+        <AppBar
+            className={classes.appbar}
+            elevation={1}
+        >
+            <Toolbar>
+                <Typography className={classes.date}>
+                   Welcome to FitStack, today is the { format(new Date(), 'do MMMM Y') }
+                </Typography>
+                <Typography>
+                    User
+                </Typography>
+                <Button><Avatar src="/default-user-image" className={classes.avatar} /></Button>
+                
+            </Toolbar>
+        </AppBar>
+        
+        {/* side drawer */}  
+        <Drawer
+            className={classes.drawer}
+            variant="permanent"
+            anchor="left"
+            classes={{ paper: classes.drawerPaper }}
+        >
+            <div>
+                <Typography variant="h5" className={classes.title}>
+                    Fitstack 
+                    <Avatar
+                        variant="round" 
+                        src="/LOGO3.png" 
+                        className={classes.avatar}
+                        sx={{ width: 24, height: 24 }} 
+                    />
+                </Typography>
+            </div>
+
+            {/* list / links */}
+            <List>
+                {menuItems.map(item => (
+                    <ListItem
+                        button
+                        key={item.text}
+                        onClick={() => history.push(item.path)}
+                        className={location.pathname == item.path ? classes.active : null}
+                    >
+                        <ListItemIcon>{item.icon}</ListItemIcon>
+                        <ListItemText primary={item.text} />
+                    </ListItem>
+                ))}
+            </List>
+
+        </Drawer>  
+        
+        <div className={classes.page}>
+            <div className={classes.toolbar}></div>
+            {children}
+        </div>
+    </div>
+  )
+}
---
+++ src/pages/Create.js
@@ -0,0 +1,130 @@
+import React, {useState} from 'react'
+import { FormControlLabel, Typography } from '@material-ui/core'
+import { Button } from '@material-ui/core'
+// import { ButtonGroup } from '@material-ui/core'
+// to create a wrapper around our content with basic margin and padding:
+import { Container } from '@material-ui/core'
+import { KeyboardArrowRight } from '@material-ui/icons'
+import { makeStyles } from '@material-ui/core'
+import { TextField } from '@material-ui/core'
+import { Radio } from '@material-ui/core'
+import { RadioGroup } from '@material-ui/core'
+import { FormControl } from '@material-ui/core'
+import { FormLabel } from '@material-ui/core'
+import { useHistory } from 'react-router-dom'
+
+// const useStyles = makeStyles({
+//   btn: {
+//     fontSize: 60,
+//     backgroundColor: "violet",
+//     "&:hover": {
+//       backgroundColor: "blue"
+//     }
+//   },
+//   title: {
+//     textDecoration: "underline",
+//     marginBottom: 20
+//   }
+// })
+
+const useStyles = makeStyles({
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: "block"
+  }
+})
+
+export default function Create() {
+  const classes = useStyles()
+  const history = useHistory()
+  const [title, setTitle] = useState("")
+  const [details, setDetails] = useState("")
+  const [titleError, setTitleError] = useState(false)
+  const [detailsError, setDetailsError] = useState(false)
+  const [category, setCategory] = useState("todos")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setTitleError(false)
+    setDetailsError(false)
+
+    if (title == "") {
+      setTitleError(true)
+    }
+    if (details == "") {
+      setDetailsError(true)
+    }
+    if (title && details) {
+      fetch("http://localhost:8000/notes", {
+        method: 'POST',
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify({ title, details, category })
+      }).then(() => history.push("/"))
+    }
+  }
+
+
+  return (
+    <Container>
+      <Typography
+        // className={classes.title}
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          onChange={(e) => setTitle(e.target.value)} 
+          className={classes.field}
+          label="Note title"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required
+          error={titleError}
+        />
+        <TextField 
+          onChange={(e) => setDetails(e.target.value)} 
+          className={classes.field}
+          label="Details"
+          variant="outlined"
+          color="secondary"
+          multiline
+          rows={4}
+          fullWidth
+          required
+          error={detailsError}
+        />
+
+        <FormControl className={classes.field}>
+          <FormLabel>Note Catergory</FormLabel>
+          <RadioGroup value={category} onChange={(e) => setCategory(e.target.value)}>
+            <FormControlLabel value="exercise" control={<Radio />} label="Exercise" />
+            <FormControlLabel value="exercise" control={<Radio />} label="Exercise" />
+            <FormControlLabel value="exercise" control={<Radio />} label="Exercise" />
+            <FormControlLabel value="exercise" control={<Radio />} label="Exercise" />
+          </RadioGroup>
+        </FormControl>
+
+        <Button
+        // className={classes.btn}
+        // onClick={() => console.log("you clicked me")}
+        type="submit"
+        color="secondary"
+        variant="contained"
+
+        endIcon={<KeyboardArrowRight />}
+        // to remove dropshadow of the button use:
+        /* disableElevation */
+      >
+        Submit
+      </Button>
+      </form>
+    </Container>
+  )
+}
